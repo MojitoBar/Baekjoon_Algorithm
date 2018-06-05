326ff37af38e20ad07b66e384c522aa22d8568b6
MojitoBar <wnehdtjr5@naver.com>
2018-06-05 14:46:42 +0900

    3문제

--- 11721/11721/source.c
+++ 11721/11721/source.c
@@ -3,18 +3,24 @@
 int main()
 {
 	char string[100] = { 0 };
-	int length = 0;
-	int sum = 0;
+	scanf("%s", &string);
 
-	scanf("%d", &length);
-	scanf("%s", string);
-
-	for (int i = 0; i < length; i++)
+	for (int i = 1; i <= 100; i++)
 	{
-		sum += string[i] - 48;
+		if (string[i - 1] != 0)
+		{
+			if (i % 10 == 0)
+			{
+				printf("%c", string[i - 1]);
+				printf("\n");
+			}
+			else
+			{
+				printf("%c", string[i - 1]);
+			}
+		}
 	}
 
-	printf("%d", sum);
 
 	return 0;
 }
