30b6639e764c17b87a20872cffa281238aa4deea
MojitoBar <wnehdtjr5@naver.com>
2018-08-04 23:06:21 +0900

    1문제

---
+++ 15680/15680/source.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	if (a == 0)
+	{
+		printf("YONSEI");
+	}
+	else if (a == 1)
+	{
+		printf("Leading the Way to the Future");
+	}
+	return 0;
+}
