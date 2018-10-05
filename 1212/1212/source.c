cd871c3b27f45d723f7c89c29550cbe88df2b058
MojitoBar <wnehdtjr5@naver.com>
2018-10-05 20:51:52 +0900

    1문제

---
+++ 1212/1212/source.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+	char arr[333334];
+	scanf("%s", &arr);
+	int len = strlen(arr);
+	for (int i = 0; i < len; i++) {
+		if (i == 0) {
+			switch (arr[i]) {
+			case '0': printf("0");
+				break;
+			case '1': printf("1");
+				break;
+			case '2': printf("10");
+				break;
+			case '3': printf("11");
+				break;
+			case '4': printf("100");
+				break;
+			case '5': printf("101");
+				break;
+			case '6': printf("110");
+				break;
+			case '7': printf("111");
+				break;
+			}
+			continue;
+		}
+		switch (arr[i]) {
+		case '0': printf("000");
+			break;
+		case '1': printf("001");
+			break;
+		case '2': printf("010");
+			break;
+		case '3': printf("011");
+			break;
+		case '4': printf("100");
+			break;
+		case '5': printf("101");
+			break;
+		case '6': printf("110");
+			break;
+		case '7': printf("111");
+			break;
+		}
+	}
+	return 0;
+}
