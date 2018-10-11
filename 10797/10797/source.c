4898b1a9af3402390b99ab217c1f5b1a999efc53
MojitoBar <wnehdtjr5@naver.com>
2018-10-11 20:00:23 +0900

    1문제

---
+++ 10797/10797/source.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+	int n, m;
+	int count = 0;
+	scanf("%d", &n);
+	for (int i = 0; i < 5; i++)
+	{
+		scanf("%d", &m);
+		if (m == n)
+		{
+			count++;
+		}
+	}
+
+	printf("%d", count);
+	return 0;
+}
