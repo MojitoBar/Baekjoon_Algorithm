cdecba2c62a72f761ae57577b7a7d2ce3079e3ab
MojitoBar <wnehdtjr5@naver.com>
2018-11-16 21:00:53 +0900

    1문제

--- ConsoleApplication1/ConsoleApplication1/source.c
+++ ConsoleApplication1/ConsoleApplication1/source.c
@@ -1,48 +1,12 @@
-#include <stdio.h>
+ï»¿#include <stdio.h>
 
-void menu();
-int add(int x, int y);
-int sub(int x, int y);
-int mul(int x, int y);
-int div(int x, int y);
+int main(void) {
+	int x, y, z;
 
-int main(void)
-{
-	int choice, result, x, y;
-	int(*pf[4])(int, int) = {add, sub, mul, div};
+	if (scanf("%d %d %d", &x, &y, &z) == 3)
+		printf("ì •ìˆ˜ë“¤ì˜ í•© = %d\n", x + y + z);
+	else
+		printf("ì…ë ¥ ì˜¤ë¥˜\n");
 
-	while (1)
-	{
-		menu();
-		printf("¸Ş´º¸¦ ¼±ÅÃÇÏ¼¼¿ä: ");
-		scanf("%d", &choice);
-
-		if (choice < 0 && choice >= 4)
-		{
-			break;
-		}
-
-		printf("2°³ÀÇ Á¤¼ö¸¦ ÀÔ·ÂÇÏ¼¼¿ä: ");
-		scanf("%d %d", &x, &y);
-
-		result = pf[choice](x, y);
-		printf("¿¬»ê °á°ú = %d\n", result);
-	}
 	return 0;
 }
-
-void menu()
-{
-	printf("======================\n");
-	printf("0. µ¡¼À\n");
-	printf("1. »¬¼À\n");
-	printf("2. °ö¼À\n");
-	printf("3. ³ª´°¼À\n");
-	printf("4. Á¾·á\n");
-	printf("======================\n");
-}
-
-int add(int x, int y) { return x + y; };
-int sub(int x, int y) { return x - y; };
-int mul(int x, int y) { return x * y; };
-int div(int x, int y) { return x / y; };
